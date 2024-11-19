d7ae8eb6ff2b7e967bf299f2240ebbbf85b39b7a
juliohtm08 <jhtomoyuki345@gmail.com>
2024-11-19 18:43:28 -0300

    clear

--- db/models/index.js
+++ db/models/index.js
@@ -1,21 +1,21 @@
 // Normatizar o código, ajuda evitar gambiarras
-'use strict';
+"use strict";
 
 // Permite trabalhar com o sistema de arquivos do computador
-const fs = require('fs');
+const fs = require("fs");
 // Fornece utilitários para trabalhar com caminhos de arquivos e diretórios
-const path = require('path');
+const path = require("path");
 // Sequelize é um ORM para Node.js, que tem suporte vários bancos de dados
 // ORM mapeamento objeto-relacional, as tabelas do banco de dados são representadas em classes e os registros das tabelas seriam instâncias dessas classes
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 // Permite obter informações do processo na página atual
-const process = require('process');
+const process = require("process");
 // Permite obter parte do caminho para o arquivo
 const basename = path.basename(__filename);
 // Verificar se deve utilizar a variável global ou 'development'
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 // Incluir o arquivo
-const config = require(__dirname + '/../config/config.js')[env];
+const config = require(__dirname + "/../config/config.js")[env];
 // Criar a constate com objeto vazio
 const db = {};
 
@@ -23,45 +23,66 @@ const db = {};
 let sequelize;
 // Verificar qual configuração de banco de dados você deseja usar
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  // Usar as configurações do arquivo "config/config.js"
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+    // Usar as configurações do arquivo "config/config.js"
+    sequelize = new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config
+    );
 }
 
 // Verificar a conexão com banco de dados
-sequelize.authenticate()
-.then(() => {
-  console.log("Conexão com o banco de dados realizado com sucesso!");
-}).catch((error) => {
-  console.log("Erro: Conexão com o banco de dados não realizado com sucesso!", error);
-});
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log("Conexão com o banco de dados realizado com sucesso!");
+    })
+    .catch((error) => {
+        console.log(
+            "Erro: Conexão com o banco de dados não realizado com sucesso!",
+            error
+        );
+    });
 
 // Identificar a MODEL
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+fs.readdirSync(__dirname)
+    .filter((file) => {
+        return (
+            file.indexOf(".") !== 0 &&
+            file !== basename &&
+            file.slice(-3) === ".js" &&
+            file.indexOf(".test.js") === -1
+        );
+    })
+    .forEach((file) => {
+        const model = require(path.join(__dirname, file))(
+            sequelize,
+            Sequelize.DataTypes
+        );
+        db[model.name] = model;
+    });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 // Atribuir a conexão com banco de dados para o objeto db
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-// Exportar a instrução que está dentro da constante db 
+// Exportar a instrução que está dentro da constante db
 module.exports = db;
+
+db.sequelize
+    .sync({ force: false })
+    .then(() => {
+        return console.log("Tabelas sincronizadas com sucesso!");
+    })
+    .catch((error) => {
+        return console.error("Erro ao sincronizar as tabelas:", error);
+    });
